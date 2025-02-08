8d750b932d09060537a7d3c22247fed82c298490
Johnnyxcode <john.ayo.jam@gmail.com>
2025-02-08 11:20:59 +0100

    added prisma to dist

--- dtos/create-user.dto.ts
+++ dtos/create-user.dto.ts
@@ -1,2 +1,34 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
+export class RegisterUserDto {
+  @IsEmail()
+  email: string = '';
 
+  @IsNotEmpty()
+  @IsString()
+  password: string = '';
+
+  @IsNotEmpty()
+  @IsString()
+  firstName: string = '';
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string = '';
+
+  @IsNotEmpty()
+  @IsString()
+  role: string = '';
+
+  @IsNotEmpty()
+  @IsString()
+  department: string = '';
+
+  @IsNotEmpty()
+  @IsString()
+  phoneNumber: string = '';
+
+  @IsOptional()
+  @IsString()
+  profilePicture?: string;
+}
