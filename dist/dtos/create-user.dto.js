c11a395198703c3c24b00669fee14a87a551cdb4
Johnnyxcode <john.ayo.jam@gmail.com>
2025-02-25 15:51:36 +0100

    built update

--- dist/dtos/create-user.dto.js
+++ dist/dtos/create-user.dto.js
@@ -11,7 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RegisterUserDto = void 0;
 const class_validator_1 = require("class-validator");
-const swagger_1 = require("@nestjs/swagger");
 class RegisterUserDto {
     constructor() {
         this.email = '';
@@ -24,54 +23,45 @@ class RegisterUserDto {
 }
 exports.RegisterUserDto = RegisterUserDto;
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User email address', example: 'user@example.com' }),
     (0, class_validator_1.IsEmail)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "email", void 0);
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User password', example: 'strongPassword123' }),
     (0, class_validator_1.IsNotEmpty)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "password", void 0);
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User first name', example: 'John' }),
     (0, class_validator_1.IsNotEmpty)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "firstName", void 0);
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User last name', example: 'Ayodele' }),
     (0, class_validator_1.IsNotEmpty)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "lastName", void 0);
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User role', example: 'Admin' }),
     (0, class_validator_1.IsNotEmpty)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "role", void 0);
 __decorate([
-    (0, swagger_1.ApiProperty)({ description: 'User department', example: 'IT' }),
     (0, class_validator_1.IsNotEmpty)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "department", void 0);
 __decorate([
-    (0, swagger_1.ApiPropertyOptional)({ description: 'User phone number', example: '+1234567890' }),
     (0, class_validator_1.IsOptional)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "phoneNumber", void 0);
 __decorate([
-    (0, swagger_1.ApiPropertyOptional)({ description: 'Profile picture URL', example: 'https://example.com/image.jpg' }),
     (0, class_validator_1.IsOptional)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
 ], RegisterUserDto.prototype, "profilePicture", void 0);
 __decorate([
-    (0, swagger_1.ApiPropertyOptional)({ description: 'Invitation token if applicable' }),
     (0, class_validator_1.IsOptional)(),
     (0, class_validator_1.IsString)(),
     __metadata("design:type", String)
--- dtos/login-user.dto.ts
+++ dtos/login-user.dto.ts
@@ -1,14 +1,9 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
 
 export class LoginUserDto {
-
-  @ApiProperty({ description: 'Login User with email address', example: 'user@example.com' })
   @IsEmail()
   email: string = '';
 
-  @ApiProperty({ description: 'Login with User password', example: 'strongPassword123' })
   @IsNotEmpty()
   @IsString()
   password: string = '';
