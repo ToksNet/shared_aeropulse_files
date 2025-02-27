a7e77d6185829797fb5062d1c2d3c5b8926e4763
Johnnyxcode <john.ayo.jam@gmail.com>
2025-02-27 11:06:20 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,11 @@ export * from '../dtos/invite-user.dto';
 export * from '../dtos/add-group-member.dto';
 export * from '../dtos/chat-message.dto';
 export * from '../dtos/create-chat-group.dto';
+export * from '../dtos/create-document.dto';
+export * from '../dtos/folder.dto';
+export * from '../dtos/update-document.dto';
+export * from '../dtos/update-folder.dto';
+export * from '../dtos/upload-documents.dto'; 
+
 
 
