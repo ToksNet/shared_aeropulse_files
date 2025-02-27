4dca7101344a292e9ae2332a3ad17703692fc2cf
Johnnyxcode <john.ayo.jam@gmail.com>
2025-02-27 04:20:37 +0100

    chat dtos

--- dtos/add-group-member.dto.ts
+++ dtos/add-group-member.dto.ts
@@ -1,12 +1,12 @@
-// // dtos/add-group-member.dto.ts
-// import { IsString, IsNotEmpty } from 'class-validator';
+// dtos/add-group-member.dto.ts
+import { IsString, IsNotEmpty } from 'class-validator';
 
-// export class AddGroupMemberDto {
-//   @IsString()
-//   @IsNotEmpty()
-//   groupId: string;
+export class AddGroupMemberDto {
+  @IsString()
+  @IsNotEmpty()
+  groupId: string = '';
 
-//   @IsString()
-//   @IsNotEmpty()
-//   userId: string;
-// }
+  @IsString()
+  @IsNotEmpty()
+  userId: string = '';
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * from '../dtos/create-user.dto';
 export * from '../dtos/login-user.dto';
 export * from '../dtos/invite-user.dto';
+export * from '../dtos/add-group-member.dto';
+export * from '../dtos/chat-message.dto';
+export * from '../dtos/create-chat-group.dto';
+
 
