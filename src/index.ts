7b9a4e589b1fe9c667a80480f87e505933ffb9ea
Johnnyxcode <john.ayo.jam@gmail.com>
2025-03-11 11:49:14 +0100

    added unique constrain to the groupmember table

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from '../dtos/user.dto';
 export * from '../dtos/chat.dto';
 export * from '../dtos/document.dto';
 export * from '../dtos/folder.dto';
-export * from '../dtos/create-signature.dto'; 
+export * from '../dtos/signature.dto'; 
 
 
 
