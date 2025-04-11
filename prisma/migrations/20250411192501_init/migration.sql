713c1ccf9c2332bd1abef6bd738b6eb341fd392b
Johnnyxcode <john.ayo.jam@gmail.com>
2025-04-11 20:33:20 +0100

    added migration file

--- prisma/migrations/20250411192501_init/migration.sql
+++ prisma/migrations/20250411192501_init/migration.sql
@@ -121,6 +121,7 @@ CREATE TABLE `ChatList` (
     `chatId` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `ChatList_userId_chatId_key`(`userId`, `chatId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
